f9cd2b92571d4e0ea58a6fe95c1589c55f405128
David Corticchiato <david.corticchiato@gmail.com>
2016-11-03 14:42:24 +0100

    recompute queryset.at(<index>)

--- test/component.test.js
+++ test/component.test.js
@@ -122,6 +122,31 @@ describe('Component', () => {
 
     });
 
+    describe('todo: Todo.objects.at(<index>)', () => {
+
+      beforeEach(() => {
+        Todo.objects.delete();
+        Todo.objects.create({text: 'build a rocket'});
+        Todo.objects.create({text: 'star dispersive'});
+
+        Component.stateFields = {
+          todo: Todo.objects.orderBy('text').at(1),
+        };
+      });
+
+      it('should initialize state.todo', () => {
+        component = new Component();
+        assert.equal(component.state.todo.text, 'star dispersive');
+      });
+
+      it('should update state.todo', () => {
+        component = new Component();
+        Todo.objects.create({text: 'feed the cat'});
+        assert.equal(component.state.todo.text, 'feed the cat');
+      });
+
+    });
+
     describe('todo: Todo.objects.all()', () => {
 
       beforeEach(() => {
