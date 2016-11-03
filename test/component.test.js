47fcc0f1cbad664e423a36ebdd679e4028248799
David Corticchiato <david.corticchiato@gmail.com>
2016-11-03 14:37:35 +0100

    recompute queryset.last()

--- test/component.test.js
+++ test/component.test.js
@@ -98,6 +98,30 @@ describe('Component', () => {
 
     });
 
+    describe('todo: Todo.objects.last()', () => {
+
+      beforeEach(() => {
+        Todo.objects.delete();
+        Todo.objects.create({text: 'star dispersive'});
+
+        Component.stateFields = {
+          todo: Todo.objects.orderBy('text').last(),
+        };
+      });
+
+      it('should initialize state.todo', () => {
+        component = new Component();
+        assert.equal(component.state.todo.text, 'star dispersive');
+      });
+
+      it('should update state.todo', () => {
+        component = new Component();
+        Todo.objects.create({text: 'wash dishes'});
+        assert.equal(component.state.todo.text, 'wash dishes');
+      });
+
+    });
+
   });
 
 });
