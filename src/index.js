af9d866ced762e55f15912a4ecda0ee77267ff26
David Corticchiato <david.corticchiato@gmail.com>
2017-02-15 11:23:26 +0100

    fix linting issues

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import {QuerySet, Model} from 'dispersive';
 import React, {Component} from 'react';
 
 
@@ -13,44 +12,41 @@ export class Observer extends Component {
    * React LifeCycle
    */
 
-   componentDidMount() {
-     this.subscriptions = {};
+  componentDidMount() {
+    this.subscriptions = {};
 
-     this.eachQuerySet(({name, value}) => this.resetQuerySetSubscription(name, value));
-     this.eachModel(({name, value}) => this.resetModelSubscription(name, value));
-   }
+    this.eachQuerySet(({name, value}) => this.resetQuerySetSubscription(name, value));
+    this.eachModel(({name, value}) => this.resetModelSubscription(name, value));
+  }
 
-   componentWillUnmount() {
-     this.eachQuerySet(({name}) => this.removeSubscription(name));
-     this.eachModel(({name}) => this.removeSubscription(name));
-   }
+  componentWillUpdate() {
+    this.eachQuerySet(({name, value}) => this.resetQuerySetSubscription(name, value));
+    this.eachModel(({name, value}) => this.resetModelSubscription(name, value));
+  }
 
-
-   componentWillUpdate() {
-     this.eachQuerySet(({name, value}) => this.resetQuerySetSubscription(name, value));
-     this.eachModel(({name, value}) => this.resetModelSubscription(name, value));
-   }
+  componentWillUnmount() {
+    this.eachQuerySet(({name}) => this.removeSubscription(name));
+    this.eachModel(({name}) => this.removeSubscription(name));
+  }
 
 
   /*
    * QuerySet subscriptions
    */
 
-   eachProps(prop, predicate) {
-     if (!this.props || !this.props[prop]) return;
+  getObserved(filter = {}) {
+    const observed = {};
+    const push = (prop, name, value) => {
+      if ((name in filter) && filter[name] === this.props[prop][name]) {
+        observed[name] = value;
+      }
+    };
 
-     Object.keys(this.props[prop]).forEach(
-       name => predicate({name, value: this.props[prop][name]})
-     );
-   }
+    this.eachQuerySet(({name, value}) => push('querysets', name, value));
+    this.eachModel(({name, value}) => push('models', name, value.objects.get({id: value.id})));
 
-   eachQuerySet(predicate) {
-     return this.eachProps('querysets', predicate);
-   }
-
-   eachModel(predicate) {
-     return this.eachProps('models', predicate);
-   }
+    return observed;
+  }
 
   removeSubscription(name) {
     if (! (name in this.subscriptions)) return;
@@ -69,23 +65,23 @@ export class Observer extends Component {
     this.subscriptions[name] = [model.changed(() => this.forceUpdate())];
   }
 
+  eachProps(prop, predicate) {
+    if (!this.props || !this.props[prop]) return;
 
-  getObserved(filter = {}) {
-    const observed = {};
-    const push = (prop, name, value) => {
-      if ((name in filter) && filter[name] === this.props[prop][name]) {
-        observed[name] = value;
-      }
-    };
+    Object.keys(this.props[prop]).forEach(
+      name => predicate({name, value: this.props[prop][name]})
+    );
+  }
 
-    this.eachQuerySet(({name, value}) => push('querysets', name, value));
-    this.eachModel(({name, value}) => push('models', name, value.objects.get({id: value.id})));
+  eachQuerySet(predicate) {
+    return this.eachProps('querysets', predicate);
+  }
 
-    return observed;
+  eachModel(predicate) {
+    return this.eachProps('models', predicate);
   }
 
   cloneElement(el) {
-    const props = el.props;
     let children = el.props.children;
 
     if (React.Children.count(children) > 0 && !isComponent(el)) {
@@ -96,18 +92,19 @@ export class Observer extends Component {
   }
 
 
-   render() {
-     const childrenCount = React.Children.count(this.props.children);
+  render() {
+    const childrenCount = React.Children.count(this.props.children);
 
-     if (childrenCount !== 1) {
-       throw new BadChildrenCount(`Observer accepts only one child. Received : ${childrenCount}`);
-     }
+    if (childrenCount !== 1) throw new Observer.BadChildrenCount(childrenCount);
 
-     return this.cloneElement(React.Children.only(this.props.children));
-   }
+    return this.cloneElement(React.Children.only(this.props.children));
+  }
 
-};
+}
 
-Observer.BadChildrenCount = function (message) {
-  Object.assign(this, ({name: 'BadChildrenCount', message}));
+Observer.BadChildrenCount = function ObserverBadChildrenCount(childrenCount) {
+  Object.assign(this, ({
+    name: 'BadChildrenCount',
+    message: `Observer accepts only one child. Received : ${childrenCount}`,
+  }));
 };
